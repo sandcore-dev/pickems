5e4a746d1d2ef540d83c7e0f51ce57c4a6386130
Sander Verbrugge <32176563+sandcore-dev@users.noreply.github.com>
2021-02-17 20:56:39 +0100

    Upgraded Laravel to 5.6

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\Validator;
@@ -17,6 +18,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Paginator::useBootstrapThree();
+
         Schema::defaultStringLength(191);
 
         Validator::extend(
