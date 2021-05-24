611faee199d708d1d0fd4f42bb6554a1d0d7d997
Sander Verbrugge <32176563+sandcore-dev@users.noreply.github.com>
2021-05-24 12:19:11 +0200

    Added ASCII table option

--- app/Console/Commands/Standings.php
+++ app/Console/Commands/Standings.php
@@ -2,9 +2,13 @@
 
 namespace App\Console\Commands;
 
+use App\Models\Standing;
 use Illuminate\Console\Command;
 use App\Models\Race;
 use App\Models\League;
+use Symfony\Component\Console\Helper\TableCell;
+use Symfony\Component\Console\Helper\TableCellStyle;
+use Symfony\Component\Console\Helper\TableStyle;
 
 class Standings extends Command
 {
@@ -13,34 +17,26 @@ class Standings extends Command
      *
      * @var string
      */
-    protected $signature = 'standings {league} {offset=0}';
+    protected $signature = 'standings {league} {offset=0}
+                                {--ascii : Output as an ASCII table instead}
+    ';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Show the standings with bbCode formatting.';
-
-    /**
-     * Create a new command instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        parent::__construct();
-    }
+    protected $description = 'Show the standings with bbCode formatting or as an ASCII table.';
 
     /**
      * Execute the console command.
      */
-    public function handle()
+    public function handle(): int
     {
         $leagueName = $this->argument('league');
-        $leagues    = League::where('name', $leagueName)->get();
-        $offset     = $this->argument('offset');
-        $race       = Race::previousOrFirst($offset);
+        $leagues = League::where('name', $leagueName)->get();
+        $offset = $this->argument('offset');
+        $race = Race::previousOrFirst($offset);
 
         if ($leagues->isEmpty()) {
             $this->error("League {$leagueName} not found.");
@@ -53,7 +49,80 @@ class Standings extends Command
 
         $standings = $league->standings->where('race_id', $race->id);
 
-        $this->line((string) view('standings.bbcode')->with('standings', $standings));
+        if ($this->option('ascii')) {
+            $number = $standings->count()
+                ? $standings->first()->race->season->picks_max
+                : config('picks.max');
+
+            $this->table(
+                [
+                    [
+                        new TableCell(
+                            '',
+                            [
+                                'colspan' => 3,
+                            ]
+                        ),
+                        new TableCell(
+                            __('Overall'),
+                            [
+                                'colspan' => 3,
+                                'style' => new TableCellStyle(['align' => 'left', 'fg' => 'green']),
+                            ]
+                        ),
+                        new TableCell(
+                            __('Race'),
+                            [
+                                'colspan' => 3,
+                                'style' => new TableCellStyle(['align' => 'left', 'fg' => 'green']),
+                            ]
+                        ),
+                    ],
+                    [
+                        __('Rank'),
+                        '+/-',
+                        __('Name'),
+                        __('Total'),
+                        __('Finish'),
+                        __('Top :number', ['number' => $number]),
+                        __('Total'),
+                        __('Finish'),
+                        __('Top :number', ['number' => $number]),
+                    ],
+                ],
+                $standings->map(
+                    function (Standing $standing) {
+                        return [
+                            $standing->rank,
+                            $standing->rank_moved,
+                            $standing->user->name,
+                            $standing->total_overall,
+                            $standing->total_positions_correct,
+                            $standing->total_picked,
+                            $standing->total,
+                            $standing->positions_correct,
+                            $standing->picked,
+                        ];
+                    }
+                ),
+                'default',
+                [
+                    (new TableStyle())->setPadType(STR_PAD_LEFT),
+                    (new TableStyle())->setPadType(STR_PAD_BOTH),
+                    (new TableStyle())->setPadType(STR_PAD_RIGHT),
+                    (new TableStyle())->setPadType(STR_PAD_LEFT),
+                    (new TableStyle())->setPadType(STR_PAD_LEFT),
+                    (new TableStyle())->setPadType(STR_PAD_LEFT),
+                    (new TableStyle())->setPadType(STR_PAD_LEFT),
+                    (new TableStyle())->setPadType(STR_PAD_LEFT),
+                    (new TableStyle())->setPadType(STR_PAD_LEFT),
+                ]
+            );
+
+            return 0;
+        }
+
+        $this->line((string)view('standings.bbcode')->with('standings', $standings));
 
         return 0;
     }
